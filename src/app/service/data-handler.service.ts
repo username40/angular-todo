8693818a35d422229fd2e811fed821e4404b41b9
username40 <mr.v.e.romantsov@gmail.com>
2021-12-13 16:10:17 +0300

    make get category and tasks list with reactive programming

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {Task} from "../model/Task";
 import {TestData} from "../data/TestData";
-import {Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  tasksSubject = new Subject<Task[]>();
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
-  constructor() { }
-
-  getCategories(): Category[] {
-    return TestData.categories
+  constructor() {
+    this.fillTasks()
   }
+
+  // getCategories(): Category[] {
+  //   return TestData.categories
+  // }
 
   fillTasks(): void {
     this.tasksSubject.next(TestData.tasks);
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -14,7 +14,7 @@ export class CategoriesComponent implements OnInit {
   constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
-    this.categories = this.dataHandler.getCategories();
+    this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
   }
 
   showTasksByCategory(category: Category) {
