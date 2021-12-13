237e256fd10f52b224c7b8f60003cb79144bf588
username40 <mr.v.e.romantsov@gmail.com>
2021-12-13 12:13:11 +0300

    add request tasks by category

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -17,4 +17,8 @@ export class CategoriesComponent implements OnInit {
     this.categories = this.dataHandler.getCategories();
   }
 
+  showTasksByCategory(category: Category) {
+    this.dataHandler.getTasksByCategory(category);
+  }
+
 }
