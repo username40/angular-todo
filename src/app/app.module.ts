b04284a5f7a9460174a795b47a88a1a7ff60c4d0
username40 <mr.v.e.romantsov@gmail.com>
2021-12-19 14:08:51 +0300

    import mat table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AppComponent} from './app.component';
-import { CategoriesComponent } from './views/categories/categories.component';
-import { TasksComponent } from './views/tasks/tasks.component';
+import {CategoriesComponent} from './views/categories/categories.component';
+import {TasksComponent} from './views/tasks/tasks.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        CategoriesComponent,
-        TasksComponent
-    ],
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    TasksComponent
+  ],
   exports: [
     CategoriesComponent,
     TasksComponent
   ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
