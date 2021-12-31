7246a5643dd99255e56468a709d40343f8972c60
username40 <mr.v.e.romantsov@gmail.com>
2021-12-31 10:16:18 +0300

    add dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,26 @@ import {CategoriesComponent} from './views/categories/categories.component';
 import {TasksComponent} from "./views/tasks/tasks.component";
 import {MatPaginatorModule, MatSortModule, MatTableModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { EditTaskDialogComponent } from './dialog/edit-task-dialog/edit-task-dialog.component';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
     declarations: [
         AppComponent,
         CategoriesComponent,
-        TasksComponent
+        TasksComponent,
+        EditTaskDialogComponent
     ],
     imports: [
         BrowserModule,
         MatTableModule,
         MatSortModule,
         MatPaginatorModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatDialogModule
     ],
     providers: [],
+    entryComponents: [EditTaskDialogComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,114 +1,136 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {
+  // AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from 'src/app/model/Task';
 import {MatTableDataSource} from "@angular/material";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
+import {EditTaskDialogComponent} from "../../dialog/edit-task-dialog/edit-task-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
-    selector: 'app-tasks',
-    templateUrl: './tasks.component.html',
-    styleUrls: ['./tasks.component.css']
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
 
 
-    // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
-    private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
-    private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+  // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+  private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
 
-    // ссылки на компоненты таблицы
-    @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
-    @ViewChild(MatSort, {static: false}) private sort: MatSort;
+  // ссылки на компоненты таблицы
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
-    private tasks: Task[];
+  private tasks: Task[];
 
-    // текущие задачи для отображения на странице
-    @Input('tasks')
-    private set setTasks(tasks: Task[]) { // напрямую не присваиваем значения в переменную, только через @Input
-        this.tasks = tasks;
-        this.fillTable();
+  // текущие задачи для отображения на странице
+  @Input('tasks')
+  private set setTasks(tasks: Task[]) { // напрямую не присваиваем значения в переменную, только через @Input
+    this.tasks = tasks;
+    this.fillTable();
+  }
+
+  @Output()
+  updateTask = new EventEmitter<Task>();
+
+  constructor(private dataHandler: DataHandlerService,
+              private dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+
+    // this.dataHandler.getAllTasks().subscribe(tasks => this.tasks = tasks);
+
+    // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+    this.dataSource = new MatTableDataSource();
+    this.fillTable(); // заполняем таблицы данными (задачи) и всеми метаданными
+  }
+
+
+  toggleTaskCompleted(task: Task): void {
+    task.completed = !task.completed;
+  }
+
+  // в зависимости от статуса задачи - вернуть цвет названия
+  private getPriorityColor(task: Task): string {
+
+    // цвет завершенной задачи
+    if (task.completed) {
+      return '#F8F9FA'; // TODO вынести цвета в константы (magic strings, magic numbers)
     }
 
-    @Output()
-    updateTask = new EventEmitter<Task>();
-
-    constructor(private dataHandler: DataHandlerService) {
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
     }
 
-    ngOnInit() {
+    return '#fff'; // TODO вынести цвета в константы (magic strings, magic numbers)
 
-        // this.dataHandler.getAllTasks().subscribe(tasks => this.tasks = tasks);
+  }
 
-        // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
-        this.dataSource = new MatTableDataSource();
-        this.fillTable(); // заполняем таблицы данными (задачи) и всеми метаданными
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+  private fillTable(): void {
+
+    if (!this.dataSource) {
+      return;
     }
 
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
 
-    toggleTaskCompleted(task: Task) {
-        task.completed = !task.completed;
-    }
+    this.addTableObjects();
 
-    // в зависимости от статуса задачи - вернуть цвет названия
-    private getPriorityColor(task: Task) {
 
-        // цвет завершенной задачи
-        if (task.completed) {
-            return '#F8F9FA'; // TODO вынести цвета в константы (magic strings, magic numbers)
+    // когда получаем новые данные..
+    // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+    // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+
+      // по каким полям выполнять сортировку для каждого столбца
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
         }
 
-        if (task.priority && task.priority.color) {
-            return task.priority.color;
+        case 'title': {
+          return task.title;
         }
+      }
+    };
 
-        return '#fff'; // TODO вынести цвета в константы (magic strings, magic numbers)
+  }
 
-    }
+  private addTableObjects(): void {
+    this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+    this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
+  }
 
-    // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
-    private fillTable() {
+  private onClickTask(task: Task) {
+    this.updateTask.emit(task);
+  }
 
-        if (!this.dataSource){
-            return;
-        }
+  private openEditTaskDialog(task: Task): void {
+    const dialogRef = this.dialog.open(EditTaskDialogComponent, {
+      data: [task, 'редактирование задачи'],
+      autoFocus: false
+    })
 
-        this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
-
-        this.addTableObjects();
-
-
-        // когда получаем новые данные..
-        // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
-        // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
-        this.dataSource.sortingDataAccessor = (task, colName) => {
-
-            // по каким полям выполнять сортировку для каждого столбца
-            switch (colName) {
-                case 'priority': {
-                    return task.priority ? task.priority.id : null;
-                }
-                case 'category': {
-                    return task.category ? task.category.title : null;
-                }
-                case 'date': {
-                    return task.date ? task.date : null;
-                }
-
-                case 'title': {
-                    return task.title;
-                }
-            }
-        };
-
-    }
-
-    private addTableObjects() {
-        this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
-        this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
-    }
-
-    private onClickTask(task: Task) {
-        this.updateTask.emit(task);
-    }
+    dialogRef.afterClosed().subscribe(result => {
+      // обработка результатов
+    })
+  }
 }
