e70618b4312324d765e7b582701864672cef3219
username40 <mr.v.e.romantsov@gmail.com>
2022-01-10 17:42:42 +0300

    edit category

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -19,6 +19,9 @@ export class CategoriesComponent implements OnInit {
     @Input()
     selectedCategory: Category;
 
+    // для отображения иконки редактирования при наведении на категорию
+    private indexMouseMove: number;
+
     constructor(private dataHandler: DataHandlerService) {
     }
 
@@ -28,7 +31,7 @@ export class CategoriesComponent implements OnInit {
     }
 
 
-    showTasksByCategory(category: Category): void {
+    private showTasksByCategory(category: Category): void {
 
         // если не изменилось значение, ничего не делать (чтобы лишний раз не делать запрос данных)
         if (this.selectedCategory === category) {
@@ -40,4 +43,16 @@ export class CategoriesComponent implements OnInit {
         // вызываем внешний обработчик и передаем туда выбранную категорию
         this.selectCategory.emit(this.selectedCategory);
     }
+
+    // сохраняет индекс записи категории, над который в данный момент проходит мышка (и там отображается иконка редактирования)
+    private showEditIcon(index: number) {
+        this.indexMouseMove = index;
+
+    }
+
+    // диалоговое окно для редактирования категории
+    private openEditDialog(category: Category) {
+        console.log(category.title);
+    }
+
 }
